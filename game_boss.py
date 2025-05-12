1c832eb8b9a1d301be9b2ef292e5c947d14ab229
xard1x <akuharenko21@gmail.com>
2025-05-12 17:04:35 +0500

    движение босса 2

--- game_boss.py
+++ game_boss.py
@@ -45,26 +45,17 @@ def boss(count):
             self.rect = self.image.get_rect()  # прозрачная подложка спрайта - физическая модель
             self.rect.x = x
             self.rect.y = y
+            self.vector = Vector2(1 , 1).normalize()
         def update(self):
-            self.rect.x += self.speed
-            if self.rect.x >= 850:
-                self.rect.y += self.speed
-            if self.rect.y >= 500:
-                self.rect.x -= self.speed
-            if self.rect.x >= 150 and self.rect.y >= 500:
-                self.rect.y -= self.speed
+            if self.rect.x <= 850 and self.rect.y >= 100:   
+                self.rect.x += self.speed *  self.vector.x
+            if self.rect.x >= 850 and self.rect.y >= 100 and  self.rect.y < 450:
+                self.rect.y += self.speed * self.vector.y
+            if self.rect.y >= 450 and self.rect.x <= 850 and self.rect.x >= 850:
+                self.rect.x += -self.vector.x * self.speed
+            if self.rect.x <= 850 and self.rect.y >= 500:
+                self.rect.y += self.vector.y * self.speed
 
-
-
-            #if self.rect.x == 850 and self.rect.y == 100:
-            #    
-            #    self.rect.y += 400
-            #if self.rect.x == 850 and self.rect.y == 500:
-            #    time.delay(4000)
-            #    self.rect.x -= 700
-            #if self.rect.x == 150 and self.rect.y == 500:
-            #    time.delay(4000)
-            #    self.rect.y -= 400
         def reset(self):
             window_game.blit(self.image, (self.rect.x, self.rect.y))
             
@@ -79,7 +70,7 @@ def boss(count):
     finish = False
 
     player = Player("bee_player.png", 500, 350, 4)
-    boss = Boss('boss.png', 150, 100, 10)
+    boss = Boss('boss.png', 150, 100, 7)
     monsters = sprite.Group()
     monsters1 = sprite.Group()
 
